6d82a7c4dd497d09e1b229cf75799714c1aa4481
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-30 11:37:08 +0600

    first commit

---
+++ bin/oop_classes.dart
@@ -0,0 +1,5 @@
+import 'package:oop_classes/oop_classes.dart' as oop_classes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oop_classes.calculate()}!');
+}
---
+++ test/oop_classes_test.dart
@@ -0,0 +1,8 @@
+import 'package:oop_classes/oop_classes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
