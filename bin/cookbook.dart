d18ae8ea30ccb115ee283a2fb94786ef3ed76603
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-12-03 13:33:17 +0600

    cookbook

---
+++ bin/cookbook.dart
@@ -0,0 +1,34 @@
+void main() {
+  classPlayground();
+}
+
+class Name {
+  final String first;
+  final String last;
+  Name(this.first, this.last);
+
+  @override
+  String toString() {
+    return '$first $last';
+  }
+}
+
+class OfficialName extends Name {
+  // Private properties begin with an underscore
+  final String _title;
+
+  // You can add colons after constructor
+  // to parse data or delegate to super
+  OfficialName(this._title, String first, String last) : super(first, last);
+
+  @override
+  String toString() {
+    return '$_title. ${super.toString()}';
+  }
+}
+
+void classPlayground() {
+  final name = OfficialName('Mr', 'Clark', 'Kent');
+  final message = name.toString();
+  print(message);
+}
